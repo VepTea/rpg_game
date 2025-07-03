ddff95dd95340d684cc3f199abe3f886bb89b0c2
VepTea <97ghddlstjs@gmail.com>
2025-07-03 17:58:11 +0900

    feat: basic battle fuction

---
+++ bin/rpg_game.dart
@@ -0,0 +1,249 @@
+import 'package:rpg_game/rpg_game.dart' as rpg_game;
+import 'dart:io'; //입력 받기 위해서 필요함.
+import 'dart:math';
+
+abstract class Object {
+  //■■■■■ abstract 클래스인 Object 정의  (Character, Monster 모두 이 클래스를 상속받음) ■■■■■
+  String name = '무명';
+  int hp = 0; //체력
+}
+
+class Character implements Object {
+  //■■■■■ Character 클래스 정의 (Object 클래스 상속받았음) ■■■■■
+  String name; //이름 나중에 입력받아서 캐릭터 객체 생성해야 됨 ㅎ
+  int hp = 50; //오버라이드 됨
+  int damage = 10; //오버라이드됨
+  int armor = 5; //방어력 멤버 변수 추가
+
+  Character(this.name); //생성자: 이름을 입력받아 초기화
+
+  void showStatus() {
+    print("$name용사님 - 체력: $hp, 공격력: $damage, 방어력: $armor");
+    //캐릭터의 상태를 출력하는 메서드
+        //호출될 위치는 RpgGame 클래스의 battle에서 player,monster 한대씩 때리고 나서 호출됨.
+  }
+
+  void attackMonster(Monster stagedMonster) {
+    // 현재 등장해 있는 몬스터의 hp변수 값을 캐릭터의 공격력만큼 빼준다.
+    print("--------------------------------------------------------");
+    print(
+      "체력 ${stagedMonster.hp}있는 ${stagedMonster.name}에게 -$damage 만큼 피해를 줬다는거 아니겠음~?",
+    );
+    //이 stagedMonster는 RpgGame 클래스의 멤버변수로 선언되어 있음.
+    stagedMonster.hp -= damage; //소환된 몬스터의 hp에서 캐릭터의 공격력만큼 감소시킨다.
+    if (stagedMonster.hp > 0) {
+      print(
+        "${stagedMonster.name}의 hp가 ${stagedMonster.hp}로 날라가 버렸다는 거 아니겠음~? 침하하~",
+      );
+      //몬스터의 hp를 출력한다.
+    } else {
+      print("${stagedMonster.name} 이거 아주 개털이 됐다~~ 말이거든요잉~!");
+      //몬스터의 hp가 0 이하가 되면 몬스터가 쓰러졌다고 출력한다.
+      // 나중에 몬스터 처치 횟수 증가시키는 로직 추가해야함.
+      // RpgGame 클래스의 monsterClearCount 변수를 1 증가시킨다.
+    }
+  }
+
+  void defend(Monster stagedMonster) {
+    print("--------------------------------------------------------");
+    print("아니 우리 용사님이~ 긍께 방어도 으잉? 아주 기깔라게 하시는구만 말이여~ 방어두 기술이다~~ 이말이여~");
+    // 너무 방어를 완벽하게 해서 오히려 체력이 상승. 말이 안되지만 원래 두곡리 퐌타지 RPG에서는 가능
+    //몬스터의 공격력은 RpgGame 클래스의 stagedMonster 객체에서 가져온것임
+    int healPoint = Random().nextInt(stagedMonster.maxDamage ~/ 3) + 1;
+    hp += healPoint; // 몬스터의 공격력의 1~ 1/3만큼 랜덤으로 체력을 회복한다.
+    print("용사님이 을~매나 편안했으면 체력을 +$healPoint 만큼 회복했다는거 아니겄슈~?");
+  } //메서드: 방어하기
+}
+
+class Monster implements Object {
+  //■■■■■ Monster 클래스 정의 (Object 클래스 상속받았음) ■■■■■
+  String name; //이름 나중에 입력받아서 몬스터 객체 생성해야 됨 ㅎ
+  int hp; //객체 생성자에서 초기화
+  int maxDamage; //랜덤으로 지정할 공격력 범위 최대값(몬스터마다 다름)
+  late int
+  damage; // 진짜 몬스터의 공격력(랜덤으로 지정됨, attackCharacter 메서드에서 매 공격시마다 새롭게 정해짐)
+
+  Monster(this.name, this.hp, this.maxDamage); //생성자: 이름, 체력, 공격력을 입력받아 초기화
+
+  void attackCharacter(Character player) {
+    //플레이어를 공격하려면 플레이어의 hp를 몬스터의 공격력만큼 감소시켜야 함.
+    //그렇다면 몬스터의 공격력을 먼저랜덤으로 정해야함.(매 턴마다 새롭게 정해야함)
+    //몬스터의 공격력의 조건: player의 방어력< 몬스터의 공격력 <= 몬스터의 최대공격력
+    damage = Random().nextInt(maxDamage); // 몬스터의 공격력을 랜덤으로 설정한다.
+    if (damage <= player.armor) {
+      //몬스터의 공격력이 플레이어의 방어력보다 낮게 나오면
+      damage = player.armor + 1;
+    } // 그냥 player의 방어력보다 +1을 더해서 설정한다.
+
+    player.hp -= (damage - player.armor); //플레이어의 hp에서 (몬스터의 공격력-플레이어 방어력)만큼 감소시킨다.
+    print("--------------------------------------------------------");
+    print("아이고오~~~!!!우리 용사님이이 글쎄~ $name에게 -$damage 만큼 공격을 그냥 받으셨는데,");
+    print("용사님이 몸이 딴딴해가지고 피가 -${damage - player.armor}밖에 안깎여가지고 ");
+    print("피통이 ${player.hp}가 남았다는거 아니겠음~?");
+    //몬스터가 플레이어를 공격했을 때 출력되는 메시지
+  }
+
+  void showStatus() {
+    //몬스터의 상태를 출력하는 메서드
+    //호출될 위치는 RpgGame 클래스의 battle에서 player,monster 한대씩 때리고 나서 호출됨.
+    print("$name - 체력: $hp, 최대공격력: $maxDamage");
+  }
+}
+
+class RpgGame {
+  Character player = Character('용사'); //■■■■■ 플레이어 객체 생성 ■■■■■ 캐릭터라는 것을 멤버로 가짐.
+  // 플레이어 이름은 '용사'로 초기화됨. 나중에 입력받아서 변경할 수 있음.
+  List<Monster> monsterList = [
+    //몬스터 목록
+    Monster('한대 맞으면 나가리 되는 말벌', 10, 25),
+    Monster('더러운 러브버그', 15, 20),
+    Monster('무시무시한 거미', 25, 20),
+    Monster('사나운 황소개구리', 30, 25),
+  ];
+  int monsterClearCount = 0;
+  //몬스터 처치 횟수, 물리친 몬스터의 갯수보다 작아야 함. 추후에 처치 횟수가 List의 length와 같아지면 게임 클리어로 간주함.■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+  bool wantsToContinue = true;
+  //게임을 계속 진행하고 싶은 의사. 몬스터 처치 후에 물어봄 이 클래스의 continue 메서드에서 변경 되는 애임.
+  late Monster //
+  stagedMonster; //소환된 몬스터를 저장할 변수. 몬스터와 전투를 할 때 사용됨. 초기값은 null로 설정됨. 나중에 getRandomMonster() 메서드에서 랜덤으로 몬스터를 소환해서 대입함.
+
+  void characterNaming() {
+    //메서드: Character 이름 등록 해주기
+    print("--------------------------------------------------------");
+    print("'만흥리 벌레 때려잡는 뒤집어지는 호들갑 용사의 킹받는 모험 시리즈 : 1 '에 오신 것을 환영합니다요");
+    while (true) {
+      print("--------------------------------------------------------");
+      print("캐릭터의 이름을 입력하시라요");
+      String? characterName = stdin.readLineSync() ?? ''; //사용자로부터 캐릭터 이름 입력 받기
+      //그런데, 입력 받은 값이 a-zA-Z가-힣일때만 바꿀 수 있게 해주고 null이나 특수문자있으면 다시 설정하라고 해야함. ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■!이건 나중에 꼭 해야함
+      try {
+        player.name = characterName; //캐릭터 이름을 입력받아서 player 객체의 name 변수에 저장
+        break; // 정상적으로 입력되면 반복문 종료
+      } catch (e) {
+        print("--------------------------------------------------------");
+        print("잘못된 입력이올시다. 다시 시도하시라요."); //예외 처리: 입력값이 null인 경우 다시 입력 받기
+      }
+    }
+    print("--------------------------------------------------------");
+    print("용사님의 존함이 ${player.name}으로 설정되었다~ 그 말이거든요잉~?");
+  }
+
+  void gameStart() {
+    //메서드: 게임 시작 해주기
+    print("--------------------------------------------------------");
+    print("게임을 시작합니다!");
+    print(
+      "${player.name} - 체력: ${player.hp} , 공격력: ${player.damage} , 방어력: ${player.armor}",
+    );
+  }
+
+  void getRandomMonster() {
+    //메서드: 몬스터 소환하기 - 몬스터 리스트에서 랜덤으로 몬스터를 선택해서 stagedMonster로 대입한다.
+    stagedMonster =
+        monsterList[Random().nextInt(
+          monsterList.length,
+        )]; //몬스터리스트에 있는 몬스터 중 랜덤으로 하나를 선택해서 stagedMonster로 대입한다.
+    print("--------------------------------------------------------");
+    print("용사님~~~살려주시라요~~~!!!");
+    print("새로운 몬스터가 나타나 위협하고 있단 말이거든요잉~~ㄷ아아아아아악!!!!꺍");
+    print(
+      "소환된 몬스터: ${stagedMonster.name} - 체력: ${stagedMonster.hp}, 최대공격력: ${stagedMonster.maxDamage}",
+    );
+  }
+
+  void battle() {
+    //메서드: 몬스터와 전투하기
+    //몬스터리스트에 랜덤으로 몬스터를 하나 선택한다.-해결함
+    //그 몬스터가 stagedMonster로 대입된다.-해결함
+    // stagedMonster와 player 객체가 전투를 시작한다.어떻게?  playerTurn()과 monsterTurn()을 반복한다.
+    //
+    // player의 턴에서는 입력값을 받아 1이면 공격 메서드를 호출하고, 2면 방어 메서드를 호출한다.
+    //        공격 메서드는 stagedMonster의 hp를 player의 공격력만큼 감소시킨다.
+    // gmae 클래스의  battle 메서드를 실행시키면,
+    // 아래가 반복된다.: (언제까지? 일단 stagedMonster가 hp< 0일때까지
+    print("--------------------------------------------------------");
+    print("전투가 시작되었단 말이거든요잉~~~!!!");
+    while (stagedMonster.hp > 0) {
+      // 무엇을 반복할거냐면 이런 거다.
+      //  player의 턴이라는 함수를 실행한다.
+      playerTurn(); // 플레이어의 턴을 실행한다
+      stagedMonster.attackCharacter(player); // 몬스터의 턴을 실행한다. 몬스터가 플레이어를 공격한다.
+      player.showStatus(); // 플레이어의 상태를 출력한다.
+      stagedMonster.showStatus(); // 몬스터의 상태를 출력한다.
+      }
+    monsterClearCount++; // 몬스터 처치 횟수를 1 증가시킨다.
+    print("--------------------------------------------------------");
+    print("축하드리옵니다 나~리~!!! 벌레를 $monsterClearCount마리 째 때려잡으셨다는 말이거든요잉~");
+  }
+
+  void playerTurn() {
+    print('${player.name}의 턴이거든요잉~~~ ');
+
+    try {
+      //일단 똑바로 안쓸 수 있으니깐, try 로 예외가 나올 수 있는 코드블록임을 인지하도록 하자
+      print("--------------------------------------------------------");
+      print("어떡하시렵니까? 용사 나으리~? (1: 공격, 2: 방어)");
+      String? input = stdin.readLineSync(); // 선택 입력 받기
+      switch (input) {
+        case '1': //1을 입력받은 경우
+          player.attackMonster(stagedMonster);
+          // 캐릭터의 attack 메서드를 실행해서 stagedMonster의 hp를 감소시킨다.
+          break; // switch문을 빠져나온다.
+        case '2': //2를 입력받은 경우
+          player.defend(stagedMonster); // 캐릭터의 defend 메서드를 실행한다.
+          break; // switch문을 빠져나온다.
+        default: // 그 외의 입력값을 받은 경우
+          print("잘못된 입력입니다. 똑바로 시도하시라요!"); // 잘못된 입력에 대한 예외 처리
+      }
+    } catch (e) {
+      print("잘못된 입력입니다. 다시 시도하시라요.");
+    }
+  }
+
+  // 	입력값 조건이 1번일 때:
+  // 	캐릭터 객체의 attack 메서드를 실행해서 game 클래스의
+  // 	stagedMonster 객체(그냥 멤버변수 중 하나)의 hp값을 바꾸고싶다.
+
+  // 	입력값 조건이 2번일 때:
+  // 	캐릭터 객체의 defend 메서드를 실행해서
+  // 	stagedMonster의 공격력의 ~/2 수를
+  // 	캐릭터 객체의 hp를 올려준다.
+
+  //플레이어의 턴이라느 함수...만드러야함.}
+
+  void continueGame() {
+    // 이 메서드 나중에 gameStart() 메서드에서 몬스터 처치 후에 호출되도록 해야겠다.■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+    while (true) {
+      //지금은 while + switch문으로 했는데, 나중에 예외 처리 try-catch문으로 바꿔야함!!!■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+      print("--------------------------------------------------------");
+      print("앞으로 더 나가볼까요 나으리~? 계속 진행하실랑가요? (y/n)");
+      String? input = stdin.readLineSync() ?? ''; //사용자로부터 입력 받기
+      switch (input) {
+        case 'y' || 'Y':
+          wantsToContinue = true; //게임을 계속 진행하고 싶은 의사(wantsToContinue)를 true로 설정
+          print("게임을 계속 진행합니다.");
+          break;
+        case 'n' || 'N':
+          wantsToContinue =
+              false; //게임을 계속 진행하고 싶은 의사(wantsToContinue)를 false로 설정
+          print("고생하셨습니다 나으리~ 게임을 저장하실랑가요잉?");
+          // 나중에 게임 저장하는 로직 추가해야함.
+          break;
+        default:
+          print("잘못된 입력입니다.");
+      }
+    }
+  }
+}
+
+void main() {
+  RpgGame game = RpgGame(); //■■■■■ 게임 객체 생성 ■■■■■
+  game.characterNaming(); //■■■■■ 캐릭터 이름 등록 메서드 호출 ■■■■■
+  game.gameStart(); //■■■■■ 게임 시작 메서드 호출 ■■■■■
+  do {
+    game.getRandomMonster(); //■■■■■ 랜덤으로 몬스터 소환 메서드■■■■■
+    game.battle();
+    game.continueGame(); // ■■■■■ 턴제 전투 시작 메서드■■■■■
+  } while (game
+      .wantsToContinue); // 게임을 계속 진행하고 싶은 의사(wantsToContinue)가 true인 동안 반복
+}
